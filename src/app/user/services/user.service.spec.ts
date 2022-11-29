f1c7da681c1fd848b09004486b33d6c54ee6af8e
MiquelBO <mboriols@hotmail.com>
2022-11-29 18:28:42 +0100

    user service unit tests

--- src/app/user/services/user.service.spec.ts
+++ src/app/user/services/user.service.spec.ts
@@ -1,90 +1,106 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { switchMap, tap } from 'rxjs';
+import { catchError, forkJoin, of, switchMap } from 'rxjs';
 
 import { UserService } from './user.service';
 import { worker } from '../../../mocks/browser';
+import { User } from '../models/user';
 
-fdescribe('UserService', () => {
+describe('UserService', () => {
   let service: UserService;
+  let davidUser: User;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule]
-    });
-    service = TestBed.inject(UserService);
-  });
-
-  
   beforeAll(async () => {
     await worker.start();
-  });
-
-  afterEach(() => {
-    worker.resetHandlers();
   });
 
   afterAll(() => {
     worker.stop();
   });
 
+  beforeEach((done) => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
+    service = TestBed.inject(UserService);
+
+    service.postUser({ name: 'david' }).pipe(
+      switchMap( id => 
+        service.getUser(id)
+      )
+    ).subscribe( data => {
+      davidUser = data;
+      done();
+    });
+  });
+
+  afterEach(() => {
+    worker.resetHandlers();
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should ...', (done) => {
-    let initialTotalUsers = 0;
+  it('should create a user when postUser is called, and get it when getUser and getAllUsers methods are called', (done) => {
+    forkJoin([
+      service.getUser(davidUser.id),
+      service.getAllUsers()
+    ]).subscribe( ([userData, users]) => {
+      expect(
+        userData.name
+      ).toEqual(davidUser.name);
+      expect(
+        users.find(user => user.id === davidUser.id)?.name
+      ).toEqual(davidUser.name);
 
-    service.getAllUsers().pipe(
-      tap(users => {
-        initialTotalUsers = users.length;
-      }),
-      // Create user david and validate
-      switchMap( _ =>
-        service.postUser({ name: 'david' })
-      ),
-      switchMap( davidId =>
-        service.getUser(davidId)
-      ),
-      tap(davidData => {
-        expect(davidData.name).toEqual('david');
-      }),
-      // Update user david and validate
-      switchMap( davidData =>
-        service.putUser({
-          ...davidData,
-          name: 'david mod'
-        })
-      ),
-      switchMap( davidId =>
-        service.getUser(davidId)
-      ),
-      tap(davidData => {
-        expect(davidData.name).toEqual('david mod');
-      }),
-      // Create user Lluis and validate
-      switchMap( _ =>
-        service.postUser({ name: 'Lluis' })
-      ),
-      switchMap( lluisId =>
-        service.getUser(lluisId)
-      ),
-      tap(lluisData => {
-        expect(lluisData.name).toEqual('Lluis');
-      }),
-      // Delete user Lluis
-      switchMap( lluisData =>
-        service.deleteUser(lluisData.id)
-      ),
-      // Check total usuers
-      switchMap( _ =>
-        service.getAllUsers()
-      ),
-      tap(users => {
-        expect(users.length).toEqual(initialTotalUsers + 1);
-      }),
-    ).subscribe( _ => {
       done();
     });
   });
+
+  it('should update a user when postUser is called', (done) => {
+    let newUserName = 'david mod';
+
+    service.putUser({
+      ...davidUser,
+      name: newUserName
+    }).pipe(
+      switchMap( _ =>
+        forkJoin([
+          service.getUser(davidUser.id),
+          service.getAllUsers()
+        ])
+      )
+    ).subscribe( ([userData, users]) => {
+      expect(
+        userData.name
+      ).toEqual(newUserName);
+      expect(
+        users.find(user => user.id === davidUser.id)?.name
+      ).toEqual(newUserName);
+
+      done();
+    });
+  });
+
+  it('should delete a user when deleteUser is called', (done) => {
+    service.deleteUser(davidUser.id).pipe(
+      switchMap( _ =>
+        forkJoin([
+          service.getUser(davidUser.id).pipe(catchError(error => of(undefined))),
+          service.getAllUsers()
+        ])
+      )      
+    ).subscribe( ([userData, users]) => {
+      expect(
+        userData
+      ).toBeUndefined();
+      expect(
+        users.find(user => user.id === davidUser.id)
+      ).toBeUndefined();
+
+      done();
+    }); 
+  });
+
 });
